2468336bb4bf86e890c81066489905098ec84468
Dominik Trojčák <trojcakd@gmail.com>
2016-04-12 13:38:21 +0200

    views

---
+++ db/Views.sql
@@ -0,0 +1,56 @@
+
+--Cena predaja a zisk z jedla
+
+create view Cena_predaja as select pre.datum
+,pre.cas
+,jed.nazov as "Nazov jedalne"
+,j.nazov as "Nazov jedla"
+,pre.POCET_KUSOV * j.CENA || ' €'  as "Celkova cena nakupu"  
+,ROUND(pre.POCET_KUSOV * j.CENA * (jed.PERCENTO_NAVYSENIA/100),2)|| ' €' as "Zisk z nakupu" 
+from  predava 
+pre inner join jedlo j 
+on pre.ID_JEDLA = j.ID_JEDLA 
+inner join jedalen jed
+on pre.ID_JEDALNE = jed.ID_JEDALNE
+order by pre.cas
+;
+
+-- Pocet jedalni podla miest
+create view Pocet_jedalni as SELECT j.MESTO, COUNT(*) AS "POCET JEDALNI" FROM JEDALEN j GROUP BY j.MESTO; 
+
+-- Najlacnejsia jedalen
+
+create view najlacnejsia_jedalen as SELECT * FROM 
+(SELECT j.MESTO, j.NAZOV, j.PERCENTO_NAVYSENIA|| ' %' as "Navysenie" FROM JEDALEN j ORDER BY j.PERCENTO_NAVYSENIA)
+ WHERE ROWNUM <= 1;
+ 
+-- Pocet muzov a zien v jedalniach
+
+create view pocet_Muzov_a_Zien as SELECT j.NAZOV, z.POHLAVIE, COUNT(*) AS "POCET" FROM ZAMESTNANEC_JEDALNE z NATURAL JOIN JEDALEN j GROUP BY z.POHLAVIE, j.NAZOV ORDER BY 1;
+
+-- Priemerny plat na poziciu
+
+create view priemerny_plat as SELECT p.NAZOV, p.HODINOVA_MZDA*120 AS "PRIEMERNA MESACNA MZDA" FROM pozicia p;
+
+-- Priemerny zisk jedalne za cele obdobie
+
+create view priemerny_zisk as SELECT j.NAZOV, AVG(pr.ZISK) AS "PRIEMERNY ZISK" FROM PRODUKCIA_ZISKU pr NATURAL JOIN JEDALEN j GROUP BY j.NAZOV;
+
+-- Menu a jedla ktore sa v nom nachadzaju
+
+create view jedla_v_menu as SELECT 
+m.NAZOV as "Nazov menu",
+j.Nazov as "Nazov jedla",
+j.gramaz,
+j.cena
+FROM OBSAHUJE o inner JOIN MENU m 
+on o.ID_MENU = m.ID_MENU
+inner join jedlo j
+on j.ID_JEDLA = o.ID_JEDLA
+order by m.nazov; 
+
+-- Oblubenost jedla
+create view oblubenost_jedla as select j.NAZOV as "Nazov jedla", sum(p.POCET_KUSOV) as "Pocet predanych ks" from predava p natural join jedlo j group by j.nazov order by SUM(p.pocet_kusov) desc;
+
+
+
